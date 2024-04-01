d2d1b01164dd8567dfde92cfe82bc06a85ac930f
tanushree-coder-girl <tanushreeahir1999@gmail.com>
2024-04-01 13:39:44 +0530

    addedd hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css'
 import {Link} from 'react-router-dom'
+import { Helmet } from 'react-helmet-async';
 
 function App() {
   return (
     <>
-     
+     <Helmet>
+<title>Learning React Helmet!</title>
+<meta name='description' content='Beginner friendly page for learning React Helmet.' />
+
+  <meta property="og:title" content=" App page inside seo og Title" />
+  <meta property="og:description" content="App page inside seo og description" />
+   <meta property="og:image"
+    content="https://fastly.picsum.photos/id/13/2500/1667.jpg?hmac=SoX9UoHhN8HyklRA4A3vcCWJMVtiBXUg0W4ljWTor7s"></meta>
+
+</Helmet>
+
       <h1>Go to Share Page</h1>
       <Link to="/sharedPage"> Go to another route </Link>
     
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,39 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 import SharePage from './pages/SharePage.tsx';
+import { HelmetProvider } from 'react-helmet-async';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
-    element: <App />,
+    element: (
+      <Suspense fallback={<></>}>
+          <App />
+      </Suspense>
+    ),
   },
-  {
-    path: '/sharedPage',
-    element: <SharePage />
-  }
+    {
+    path: "/sharedPage",
+    element: (
+      <Suspense fallback={<></>}>
+          <SharePage />
+      </Suspense>
+    ),
+  },
 ]);
+const helmetContext = {};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+<HelmetProvider context={helmetContext}>
     <RouterProvider router={router} />
+
+</HelmetProvider>
   </React.StrictMode>,
 )
