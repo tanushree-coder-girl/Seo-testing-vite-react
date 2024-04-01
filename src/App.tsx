82747983d5d8c1802c8d585283f2909c11d0e16e
tanushree-coder-girl <tanushreeahir1999@gmail.com>
2024-04-01 14:44:16 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import './App.css'
-import {Link} from 'react-router-dom'
+import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <meta property="og:title" content="Your Page Title" />
+      <meta
+        property="og:description"
+        content="Description of your page content"
+      />
+      <meta
+        property="og:image"
+        content="https://fastly.picsum.photos/id/137/200/300.jpg?hmac=5vAnK2h9wYgvt2769Z9D1XYb8ory9_zB0bqDgVjgAnk"
+      />
+      <meta property="og:url" content="https://react-vite-seo.netlify.app/" />
+      <meta property="og:type" content="website" />
       <h1>Go to Share Page</h1>
       <Link to="/sharedPage"> Go to another route </Link>
-    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
