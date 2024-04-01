0226b0b8ca98127f0c77ed894215ecdcc4dfe6dd
tanushree-coder-girl <tanushreeahir1999@gmail.com>
2024-04-01 14:49:40 +0530

    content

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import {Helmet} from "react-helmet-async";
 import "./App.css";
 import { Link } from "react-router-dom";
 
 function App() {
   return (
     <>
+    <Helmet>
       <meta property="og:title" content="Your Page Title" />
       <meta
         property="og:description"
@@ -15,6 +17,7 @@ function App() {
       />
       <meta property="og:url" content="https://react-vite-seo.netlify.app/" />
       <meta property="og:type" content="website" />
+      </Helmet>
       <h1>Go to Share Page</h1>
       <Link to="/sharedPage"> Go to another route </Link>
     </>
