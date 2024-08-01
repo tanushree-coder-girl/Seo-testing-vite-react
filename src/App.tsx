283307155b01fa78b7fc990215b3fdb1cb94db08
tanushree-coder-girl <tanushreeahir1999@gmail.com>
2024-08-01 17:01:53 +0530

    commit this

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 const App = () => {
   return (
     <>
-      <button onClick={()=>console.log('hello world')}>click me</button>
+      <button style={{cursor:'pointer'}} onClick={()=>console.log('hello world')}>click me</button>
+
+{/* <div className="plugin-web-update-notice-anchor">
+  <div className="plugin-web-update-notice">
+    <div className="plugin-web-update-notice-content" data-cy="notification-content">
+      <div className="plugin-web-update-notice-content-title">
+        📢  system update
+      </div>
+      <div className="plugin-web-update-notice-content-desc">
+        System update, please refresh the page
+      </div>
+      <div className="plugin-web-update-notice-tools">
+        <a className="plugin-web-update-notice-btn plugin-web-update-notice-refresh-btn">
+          refresh
+        </a>
+      </div>
+    </div>
+  </div>
+</div> */}
     </>
   )
 }
