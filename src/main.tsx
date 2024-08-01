b7d34cce3837e211ced49115f3edc898ea9a796c
tanushree-coder-girl <tanushreeahir1999@gmail.com>
2024-08-01 13:35:35 +0530

    vite update notification

--- src/main.tsx
+++ src/main.tsx
@@ -1,48 +1,9 @@
-import React, {Suspense} from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import {
-  createHashRouter,
-  RouterProvider,
-} from "react-router-dom";
-import SharePage from './pages/SharePage.tsx';
-import { HelmetProvider } from 'react-helmet-async';
-import VideoPlayer from './pages/VideoPlayer.tsx';
-import Neww from './pages/Newww.tsx';
-
-const router = createHashRouter([
-  {
-    path: "/",
-    element: (
-      <Suspense fallback={<></>}>
-          <Neww />
-      </Suspense>
-    ),
-  },
-    {
-    path: "/sharedPage",
-    element: (
-      <Suspense fallback={<></>}>
-          <SharePage />
-      </Suspense>
-    ),
-  },
-     {
-    path: "/video",
-    element: (
-      <Suspense fallback={<></>}>
-          <VideoPlayer />
-      </Suspense>
-    ),
-  },
-]);
-const helmetContext = {};
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-<HelmetProvider context={helmetContext}>
-    <RouterProvider router={router} />
-
-</HelmetProvider>
+    <App />
   </React.StrictMode>,
 )
